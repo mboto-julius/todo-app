ad46e9a679c2a0cdaaa6cc9ab6ad6daf495ae3a3
Julius Mboto <juliusmboto@gmail.com>
2024-07-13 18:35:10 +0300

    feat: displaying data in vue js component

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="list-group-item py-3">
+        <div class="d-flex justify-content-start align-items-center">
+            <input class="form-check-input mt-0 completed" type="checkbox" />
+            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+                <!-- <div class="relative">
+                    <input class="editable-task" type="text" />
+                </div> -->
+                <span>{{ task.name }}</span>
+            </div>
+            <div class="task-date">24 Feb 12:00</div>   
+        </div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+                <IconPencil />
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger">
+                <IconTrash />
+            </button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+import IconPencil from "../icons/IconPencil.vue"
+import IconTrash from "../icons/IconTrash.vue"
+
+defineProps({
+    task: Object
+})
+</script>
