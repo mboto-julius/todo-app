785403e33936a34ef32ce7e0045440286619dc8e
Julius Mboto <juliusmboto@gmail.com>
2024-07-16 04:26:30 +0300

    feat: creating inline editing feature

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -2,33 +2,43 @@
     <li class="list-group-item py-3">
         <div class="d-flex justify-content-start align-items-center">
             <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
-            <div class="ms-2 flex-grow-1" :class="completedClass" title="Double click the text to edit or remove">
-                <!-- <div class="relative">
-                    <input class="editable-task" type="text" />
-                </div> -->
-                <span>{{ task.name }}</span>
+            <div class="ms-2 flex-grow-1" 
+                :class="completedClass" 
+                title="Double click the text to edit or remove"
+                @dblclick="$event => isEdit = true"
+            >
+                <div class="relative" v-if="isEdit">
+                    <input class="editable-task" 
+                        type="text" 
+                        @keyup.esc="$event => isEdit = false"
+                        v-focus
+                    />
+                </div>
+                <span v-else>{{ task.name }}</span>
             </div>
             <div class="task-date">24 Feb 12:00</div>   
         </div>
-        <div class="task-actions">
-            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
-                <IconPencil />
-            </button>
-            <button class="btn btn-sm btn-circle btn-outline-danger">
-                <IconTrash />
-            </button>
-        </div>
+
+        <!-- Task action i.e remove and edit action -->
+        <TaskActions @edit="$event => isEdit = true" v-show="!isEdit"/>
+
     </li>
 </template>
 
 <script setup>
-import { computed } from "vue"
-import IconPencil from "../icons/IconPencil.vue"
-import IconTrash from "../icons/IconTrash.vue"
+
+import { computed, ref } from "vue"
+import TaskActions from "../tasks/TaskActions.vue"
 
 const props = defineProps({
     task: Object
 })
 
+const isEdit = ref(false)
 const completedClass = computed(() => props.task.is_completed ? "completed" : "")
+
+const vFocus = {
+    mounted: (el) => el.focus()
+}
+
 </script>
---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="task-actions">
+        <button class="btn btn-sm btn-circle btn-outline-secondary me-1" @click="$event => emit('edit')">
+            <IconPencil />
+        </button>
+        <button class="btn btn-sm btn-circle btn-outline-danger" @click="$event => emit('remove')">
+            <IconTrash />
+        </button>
+    </div>
+</template>
+
+<script setup>
+
+import IconPencil from "../icons/IconPencil.vue"
+import IconTrash from "../icons/IconTrash.vue"
+
+const emit = defineEmits(['edit', 'remove'])
+
+</script>
