2199b93e853e78379ce1f4c15d9d96b6faca059b
Julius Mboto <juliusmboto@gmail.com>
2024-07-18 05:32:05 +0300

    feat: refactoring our store

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,25 +1,20 @@
+import { ref, reactive, computed } from "vue";    
 import { defineStore } from "pinia";
-import { allTasks} from "../http/task-api.js"
+import { allTasks} from "../http/task-api.js";
 
-export const useTaskStore = defineStore('taskStore', {
-    state: () => ({
-        tasks: [],
-        task: {
-            id: 1,
-            name: "First task",
-            is_completed: false
-        }
-    }),
-    getters: {
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        uncompletedTasks () {
-            return this.tasks.filter(task => !task.is_completed)
-        }
-    },
-    actions: {
-        async fetchAllTasks() {
-            const  { data } = await allTasks()
-            this.tasks = data.data 
-        }
+export const useTaskStore = defineStore('taskStore', () => {
+    const tasks = ref([]); 
+
+    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+
+    const fetchAllTasks = async() => {
+        const  { data } = await allTasks()
+        tasks.value = data.data 
     }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
+    }
+
 })
