6a63f1ba3c6cc6bb3eafdcca652b3995dcd32050
Julius Mboto <juliusmboto@gmail.com>
2024-08-14 12:53:27 +0300

    feat: adding a register form

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,5 +1,74 @@
 <template>
-    <div class="text-center pt-5">
-        <h3>Register Page</h3>
-    </div>
+    <main class="auth-wrapper">
+        <form class="auth-form" @submit.prevent="handleSubmit">
+            <h1>
+                <span>ToDo</span>
+                <strong>List</strong>
+            </h1>
+            <h2 class="h3 mb-4 fw-normal">Please sign up</h2>
+            <div class="form-floating mb-2">
+                <input type="text" class="form-control" :class="{ 'is-invalid': errors.name && errors.name[0] }" id="name" v-model="form.name" placeholder="Julius Mboto" />
+                <label for="name">Name</label>
+                <div class="invalid-feedback" v-if="errors.name && errors.name[0]">{{ errors.name && errors.name[0] }}</div>
+            </div>
+            <div class="form-floating mb-2">
+                <input type="email" class="form-control" :class="{ 'is-invalid': errors.email && errors.email[0] }" id="email" v-model="form.email" placeholder="juliusmboto@gmail.com" />
+                <label for="email">Email</label>
+                <div class="invalid-feedback" v-if="errors.email && errors.email[0]">{{ errors.email && errors.email[0] }}</div>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" :class="{ 'is-invalid': errors.password && errors.password[0] }" id="password" v-model="form.password" placeholder="Password" />
+                <label for="password">Password</label>
+                <div class="invalid-feedback" v-if="errors.password && errors.password[0]">{{ errors.password && errors.password[0] }}</div>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="password_confirmation" v-model="form.password_confirmation" placeholder="Password Confirmation" />
+                <label for="password">Password Confirmation</label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+        </form>
+    </main>
 </template>
+
+<script setup>
+
+import { reactive } from "vue"; 
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+
+const router = useRouter()
+const store = useAuthStore()
+const { isLoggedIn, errors } = storeToRefs(store)
+const { handleRegister } = store
+
+const form = reactive({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+})
+
+const handleSubmit = async () => {
+    await handleRegister(form)
+    if(isLoggedIn.value){
+        router.push({ name: 'tasks' })
+    }
+}
+
+</script>
+
+<style scoped>
+.auth-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 60vh;
+    margin-top: 2rem;
+}
+.auth-form {
+    width: 400px;
+}
+</style>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -32,11 +32,17 @@ export const useAuthStore = defineStore("authStore", () => {
     }
 
     const handleRegister = async (newUser) => {
-        await register(newUser)
-        await handleLogin({
-            email: newUser.email,
-            password: newUser.password
-        })
+        try {
+            await register(newUser)
+            await handleLogin({
+                email: newUser.email,
+                password: newUser.password
+            })
+        } catch (error) {
+            if (error.response && error.response.status === 422){
+                errors.value = error.response.data.errors
+            }
+        }
     }
 
     const handleLogout = async () => {
